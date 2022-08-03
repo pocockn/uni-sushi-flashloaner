e0edba97b40beb144110ff8770f53ad3e2e08602
Nick Pocock <pocockn@hotmail.co.uk>
2022-08-03 15:54:19 +0200

    Comments

--- index.js
+++ index.js
@@ -14,33 +14,35 @@ const addresses = require('./addresses/ethereum');
 
 // use your own Infura node in production
 // const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_KEY);
-const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_KEY);
+const provider = new ethers.providers.InfuraProvider('goerli', process.env.INFURA_KEY);
 
 const wallet = new ethers.Wallet(privateKey, provider);
 
+// Work out if we can fetch these values from Uniswap themselves.
+// These are the amounts we try to swap. This guy gets the amounts using this
+// https://github.com/6eer/uniswap-sushiswap-arbitrage-bot/blob/300db222e20070fb3ed488cfb0a6dcb476aea833/src/bot_flashswap.js#L110
 const ETH_TRADE = 10;
 const DAI_TRADE = 3500;
 
 const runBot = async () => {
   const sushiFactory = new ethers.Contract(
-    '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
-    // testnet '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+    // mainnet '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
+    '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
     UniswapV2Factory.abi, wallet,
   );
   const uniswapFactory = new ethers.Contract(
     '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-    // testnet '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
     UniswapV2Factory.abi, wallet,
   );
 
-  const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-  // testnet const wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+  // const mainnet wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+  const wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
 
   provider.on('block', async (blockNumber) => {
     try {
       console.log(blockNumber);
 
-      await Promise.all(addresses.tokens.map(async (token) => {
+      await Promise.all(addresses.testTokens.map(async (token) => {
         const sushi = new ethers.Contract(
           sushiFactory.getPair(wethAddress, token.address),
           UniswapV2Pair.abi, wallet,
@@ -55,7 +57,6 @@ const runBot = async () => {
         const uniswapReserves = await uniswap.getReserves();
 
         const reserve0Sushi = Number(ethers.utils.formatUnits(sushiReserves[0], token.decimal));
-
         const reserve1Sushi = Number(ethers.utils.formatUnits(sushiReserves[1], token.decimal));
 
         const reserve0Uni = Number(ethers.utils.formatUnits(uniswapReserves[0], token.decimal));
@@ -81,16 +82,19 @@ const runBot = async () => {
 
         if (!shouldTrade) return;
 
-        const gasLimit = await sushi.estimateGas.swap(
-          !shouldStartEth ? DAI_TRADE : 0,
-          shouldStartEth ? ETH_TRADE : 0,
-          flashLoanerAddress,
-          ethers.utils.toUtf8Bytes('1'),
-        );
+        // TODO: Trying to estimate the gas failed so I manually set it below for now
+        // const gasLimit = await sushi.estimateGas.swap(
+        //   !shouldStartEth ? DAI_TRADE : 0,
+        //   shouldStartEth ? ETH_TRADE : 0,
+        //   flashLoanerAddress,
+        //   ethers.utils.toUtf8Bytes('1'),
+        // );
+
+        const gasLimit = 100000;
 
         const gasPrice = await wallet.getGasPrice();
 
-        const gasCost = Number(ethers.utils.formatEther(gasPrice.mul(gasLimit)));
+        const gasCost = Number(ethers.utils.formatEther(gasPrice.mul(4250000006)));
 
         const shouldSendTx = shouldStartEth
           ? (gasCost / ETH_TRADE) < spread
@@ -103,8 +107,9 @@ const runBot = async () => {
           gasPrice,
           gasLimit,
         };
+
         const tx = await sushi.swap(
-          !shouldStartEth ? DAI_TRADE : 0,
+          !shouldStartEth ? 4000 : 0,
           shouldStartEth ? ETH_TRADE : 0,
           flashLoanerAddress,
           ethers.utils.toUtf8Bytes('1'), options,
